1c81cefe96b639eb78461e43915e6ba84d4218cb
zhaoshixin <zhaoshixin@xmeise.com>
2018-08-09 17:35:19 +0800

    feat:complete handle_softimages feature

--- examples/python-sql/handle_qrcode.py
+++ examples/python-sql/handle_qrcode.py
@@ -14,6 +14,7 @@ import qrcode
 import pyqrcode
 import mysql.connector
 import re
+import json
 import os
 
 PATH = "./qrcode"
@@ -34,38 +35,66 @@ config = {
   'use_pure': False,
 }
 
-cnx = mysql.connector.connect(**config)
 
 def select():
+    cnx = mysql.connector.connect(**config)
     cursor = cnx.cursor(dictionary =True)
-
     query = ("SELECT id,downfiles FROM handle_soft where length(downfiles)<>0")
-
     cursor.execute(query)
     rows = cursor.fetchall()
+    cursor.close()
+    cnx.close()
+    return rows
+
+def update():
+    cnx = mysql.connector.connect(**config)
+    cursor = cnx.cursor()
+    fileurl2 = handle_download_url()
+    for row in fileurl2:
+        for (k,v) in row.items():
+            query = "update handle_soft set downfiles=%(url)s where id=%(id)s"
+            x = cursor.execute(query,{'url':v,'id':k})
+            cnx.commit()
+            print(x,cursor.statement)
+        
+            # print(query,x,k,v)
+    cursor.close()
+    cnx.close()
+
+def handle_softimage():
+    cnx = mysql.connector.connect(**config)
+    cursor = cnx.cursor()
+    query = "select id,softimage from handle_software_data"
+    cursor.execute(query)
+    rows = cursor.fetchall()
+    for id,softimage in rows:
+        pattern = '(/.*?\.(?:jpg|gif|png|jpeg))'
+        imgs = re.compile(pattern)
+        imgs = imgs.findall(softimage)
+        query = "update handle_software_data set softimage=%(imgs)s where id=%(id)s"
+        x = cursor.execute(query,{'imgs':json.dumps(imgs),'id':id})
+        print(x,cursor.statement)
+        cnx.commit()
+        # print(id,imgs)
+    cursor.close()
+    cnx.close()
+
+def handle_download_url():
+    rows = select();
+    fileurl = fileurl1 = fileurl2 = fileurl3 = fileurl4 = []
     for row in rows:
         downfiles = row['downfiles']
         pattern = '((?:https?://|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}/)(?:[^\s()<>]|\(([^\s()<>]+|(\([^\s()<>]+\)))*\))+(?:\(([^\s()<>]+|(\([^\s()<>]+\)))*\)|[^\s`!()\[\]{};:\'\"\\\/.,<>?\xab\xbb\u201c\u201d\u2018\u2019]))'
         urls = re.compile(pattern)
         urls = urls.findall(downfiles)
-        url = urls.pop()
         if len(urls):
             url = urls.pop()
-            handle = dict(row['id'],url[0])
-            print(handle)
+            fileurl2.append({row['id']:url[0]})
         else:
             continue
-        print(url[0])
-        
-
-    cursor.close()
-    cnx.close()
-
-# def insert():
-    
-# def handle_download_url():
+    return fileurl2
     
 
 if __name__ == "__main__":
     # execute only if run as a script
-    select() 
+    handle_softimage() 
