5e934525c3d44d3bda29717f1461117e7c376063
shanezhiu <shanezhiu@gmail.com>
2018-08-10 18:28:50 +0800

    feat:下载类型

--- examples/python-sql/handle_qrcode.py
+++ examples/python-sql/handle_qrcode.py
@@ -4,13 +4,14 @@
 ## 数据库处理需求
 # 1. 将ttrar数据导入到3xiazai保持id自增,注意分类对应
 # 2. 处理下载链接
-#   a. 如果downfile2 不为空，则 pc_down_url=download.ttrar.com/ + downfile2
+#   a. 如果downfile2 不为空，则 pc_down_url=//download.ttrar.com/ + downfile2
 #   b. 如果downfile2为空，ycdownload 不为空，则pc_down_url=ycdownload
-#   c. 如果downfile2为空，ycsownload 为空，downloadfiles不为空，则pc_down_url=downloadfiles[0]['fileurl']
-#   d. 如果downfile2为空，ycsownload 为空，downloadfiles为空,则pc_down_url=title_url + @ + title
+#   c. 如果downfile2为空，ycsownload 为空，downfiles不为空，则pc_down_url=downfiles[0]['fileurl']
+#   d. 如果downfile2为空，ycsownload 为空，downfiles为空,则pc_down_url=title_url + @ + title
 # 3. 生成二维码并且更新二维码图片md5值到数据库.
 # 4. 区分是pc_down_url,ios_down_url,android_down_url,插入到线上表
 # 5. 根据 article表，需要生成一张tiny表，生成层级关系
+# 6. 为软件生成m站，20*20的二维码
 
 ## 需要生成的表字段
 # id,
@@ -102,6 +103,23 @@ def handle_softimage():
     cursor.close()
     cnx.close()
 
+# 给每张图片拼接https://www.ttrar.com/ host
+def handle_image_with_host():
+    cnx = mysql.connector.connect(**config)
+    cursor = cnx.cursor()
+    query = "select id,softimage from handle_software_data where id<=102158"
+    cursor.execute(query)
+    rows = cursor.fetchall()
+    for id,softimage in rows:
+        handle_img = []
+        # print(json.loads(softimage))
+        for img in json.loads(softimage):
+           handle_img.append(img.replace('http','https'))
+        query = "update handle_software_data set softimage=%(imgs)s where id=%(id)s"
+        x = cursor.execute(query,{'imgs':json.dumps(handle_img),'id':id})
+        print(x,cursor.statement)
+        cnx.commit()
+
 def handle_download_url():
     rows = select();
     fileurl = fileurl1 = fileurl2 = fileurl3 = fileurl4 = []
@@ -125,9 +143,65 @@ def generate_temporary_table():
     rows = cursor.fetchall()
     print(rows,cursor.statement)
 
+def differ_pc_or_app():
+    cnx = mysql.connector.connect(**config)
+    cursor = cnx.cursor(dictionary=True)
+    query = "select s.id,downfile2,ycdownload, downfiles,concat('//22859.xc.05cg.com/xiaz/',title,'@',s.filename) lurl,title from handle_software_data d left join handle_software s on d.id=s.id"
+    cursor.execute(query)
+    rows = cursor.fetchall()
+
+    for row in rows:
+        if row['downfile2'].strip():
+            down = "//download.ttrar.com/small/" + row['downfile2']
+            if re.search('[0-9a-zA-Z]+\.apk',down):
+                x = "update handle_software_data set android_down_url=%(down)s where id= %(id)s"
+                cursor.execute(x,{"down":down,"id":row['id']})
+                cnx.commit()
+                # print(cursor.statement)
+            else:
+                x = "update handle_software_data set pc_down_url=%(down)s where id= %(id)s"
+                cursor.execute(x,{"down":down,"id":row['id']})
+                cnx.commit()
+                # print(cursor.statement)
+        elif row['ycdownload'].strip():
+            down = row['ycdownload'].replace('http:','')
+            down = down.replace('https:','')
+            if re.search('[0-9a-zA-Z]+\.apk',down):
+                x = "update handle_software_data set android_down_url=%(down)s where id= %(id)s"
+                cursor.execute(x,{"down":down,"id":row['id']})
+                cnx.commit()
+                # print(cursor.statement)
+            else:
+                x = "update handle_software_data set pc_down_url=%(down)s where id= %(id)s"
+                cursor.execute(x,{"down":down,"id":row['id']})
+                cnx.commit()
+                # print(cursor.statement)
+        elif row['downfiles'].strip():
+            down = row['downfiles'].replace('http:','')
+            down = down.replace('https:','')
+            if re.search('[0-9a-zA-Z]+\.apk',down):
+                x = "update handle_software_data set android_down_url=%(down)s where id= %(id)s"
+                cursor.execute(x,{"down":down,"id":row['id']})
+                cnx.commit()
+                print(cursor.statement)
+            else:
+                x = "update handle_software_data set pc_down_url=%(down)s where id= %(id)s"
+                cursor.execute(x,{"down":down,"id":row['id']})
+                cnx.commit()
+                print(cursor.statement)
+        else:
+            down = row['lurl']
+            x = "update handle_software_data set pc_down_url=%(down)s where id= %(id)s"
+            cursor.execute(x,{"down":down,"id":row['id']})
+            cnx.commit()
+            print(cursor.statement)  
+            
+                
+            
+
 # def generate_tiny_table():
 
 # def insert_to_online():
 if __name__ == "__main__":
     # execute only if run as a script
-    generate_temporary_table() 
+    differ_pc_or_app() 
