7339017962e1297500585ac7b5e231f498599ed9
shanezhiu <shanezhiu@gmail.com>
2018-08-28 18:56:06 +0800

    refactor: add new requirements and a select method

--- examples/python-xiazai/handle_xiazai.py
+++ examples/python-xiazai/handle_xiazai.py
@@ -4,6 +4,7 @@
 # 需求：
 # 1. 生成一个临时表,表中初步跟xz_article字段一致
 # 2. 更新一下临时表中的分类信息
+# 3. 
 import qrcode
 import pyqrcode
 import hashlib 
@@ -26,7 +27,10 @@ config = {
 def select():
     cnx = mysql.connector.connect(**config)
     cursor = cnx.cursor(dictionary =True)
-    query = ("select id,catid,title,concat() from v9_download d left join v9_download_data a on d.id=a.id")
+    query = ("select d.id,catid,title,thumb img_md5,keywords,description,url old_url, 1 status," 
+    "'未进化的类人猿' writer, '未进化的类人猿' approve_person,FROM_UNIXTIME(UNIX_TIMESTAMP(),'%y-%m-%d %H:%m:%s')"
+    ",language,version,filesize,score star,filename,sub_title,self_title,hit_views count,content body,downfiles"
+    ",dbycdownload,bddownload,softimage,download_tips from v9_download d left join v9_download_data a on d.id=a.id")
     cursor.execute(query)
     rows = cursor.fetchall()
     for row in rows:
